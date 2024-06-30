05b0d28d76f396259e385cc277f7f0d967cf52c5
AbhiJeeT_Ramteke <abhijeetramteke89@gmail.com>
2024-06-30 07:48:44 +0530

    assignmet

---
+++ src/main/scala/ans9.scala
@@ -0,0 +1,11 @@
+object ans9 {
+  def main(args: Array[String]): Unit = {
+    var a=35
+    if(a%5==0 && a%7==0){
+      print("Number is divisible by 5 and 7")
+    }else{
+      print("Number is not divisible by 5 and 7")
+    }
+  }
+
+}
