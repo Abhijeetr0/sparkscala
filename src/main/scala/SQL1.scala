dcd8f2688659a1eda3fb1816f2ac23d6d3cb726f
AbhiJeeT_Ramteke <abhijeetramteke89@gmail.com>
2024-07-24 19:40:20 +0530

    SQLassignment

--- src/main/scala/SQL1.scala
+++ src/main/scala/SQL1.scala
@@ -2,4 +2,4 @@ SELECT employees.name, dept_name
 FROM employees
   INNER JOIN departments
 ON employees.dept_id = Departments.dept_id
-WHERE Departments.dept_name = Engineering;
+WHERE Departments.dept_name= Engineering;
