0b8232d964d244aa4af37005de600857a2453a2f
Abhijeetr0 <111679554+Abhijeetr0@users.noreply.github.com>
2024-09-29 19:11:51 +0530

    Synchronizing job 2ndjob from AWS Glue at 2024/09/29 13:41:50

---
+++ 2ndjob/2ndjob.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+df = spark.read.format("csv").option("header",True).load("s3://sep5/details.csv")
+
+df1 = df.write.format("csv").option("header",True).save("s3://awsbucket1010/info/")
+df.show()
+job.commit()
